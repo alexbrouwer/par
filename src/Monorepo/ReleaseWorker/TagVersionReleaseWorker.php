07a0e56015e94579fe30943cfe9b658b6c499220
Alex Brouwer <alexander@socialdeal.nl>
2020-04-02 13:32:43 +0200

    Added services for custom monorepo-builder release flow and disabled the default

---
+++ src/Monorepo/ReleaseWorker/TagVersionReleaseWorker.php
@@ -0,0 +1,40 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace App\Monorepo\ReleaseWorker;
+
+use PharIo\Version\Version;
+use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
+use Symplify\MonorepoBuilder\Release\Process\ProcessRunner;
+use Throwable;
+
+final class TagVersionReleaseWorker implements ReleaseWorkerInterface {
+
+    /**
+     * @var ProcessRunner
+     */
+    private $processRunner;
+
+    public function __construct ( ProcessRunner $processRunner ) {
+        $this->processRunner = $processRunner;
+    }
+
+    public function getPriority (): int {
+        return 400;
+    }
+
+    public function work ( Version $version ): void {
+        try {
+            $this->processRunner->run( 'git add . && git commit -m "prepare release" && git push origin master' );
+        } catch ( Throwable $throwable ) {
+            // nothing to commit
+        }
+
+        $this->processRunner->run( 'git tag ' . $version->getVersionString() );
+    }
+
+    public function getDescription ( Version $version ): string {
+        return sprintf( 'Add local tag "%s"', $version->getVersionString() );
+    }
+}
